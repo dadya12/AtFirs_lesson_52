57323856dfffbab564947140e845e0df899f2b5d
Adam <adam.nemmish213@gmail.com>
2024-04-30 20:14:21 +0600

    Немного переделал логику

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from webapp.views import home_page
+from webapp.views import home, task_page
 
 urlpatterns = [
-    path('', home_page, name='home')
+    path('', home, name='home'),
+    path('task_page/', task_page, name='task_page')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,21 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from webapp.models import Tasks
 
-def home_page(request):
+def task_page(request):
     tasks = Tasks.objects.all()
-    return render(request, 'home_page.html', {'tasks': tasks})
+    return render(request, 'task_page.html', {'tasks': tasks})
+
+def home(request):
+    return render(request, 'home.html')
+
+def task_create(request):
+    if request.method == 'GET':
+        return render(request, 'task_create.html')
+    elif request.method == "POST":
+        Tasks.objects.create(
+            description=request.POST['description'],
+            status=request.POST['status'],
+            date_done=request.POST['date_done']
+        )
+        return HttpResponseRedirect('/')
