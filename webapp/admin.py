0ff45ad7524d9aefa941b56e67f158ce3075230f
Adam <adam.nemmish213@gmail.com>
2024-04-30 19:16:20 +0600

    Описал модели и зарегестрировал в админке

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from webapp.models import Tasks
 
-# Register your models here.
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'date_done']
+    list_filter = ['status']
+    search_fields = ['status', 'id']
+    fields = ['description', 'status', 'date_done']
+
