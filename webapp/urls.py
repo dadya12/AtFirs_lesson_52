6623e4f576deadeda54fa71313f1d7dcd0af0b52
Adam <adam.nemmish213@gmail.com>
2024-04-30 20:59:05 +0600

    Написал функцию по добавлению задачи, так же написал шаблон и написал проверку если дата остается пустой строкой

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from webapp.views import home, task_page
+from webapp.views import home, task_page, task_create
 
 urlpatterns = [
     path('', home, name='home'),
-    path('task_page/', task_page, name='task_page')
+    path('task_page/', task_page, name='task_page'),
+    path('task_create/', task_create, name='task_create')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,21 +1,27 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from webapp.models import Tasks
+from webapp.models import Tasks, status_choices
+
 
 def task_page(request):
     tasks = Tasks.objects.all()
     return render(request, 'task_page.html', {'tasks': tasks})
 
+
 def home(request):
     return render(request, 'home.html')
 
+
 def task_create(request):
     if request.method == 'GET':
-        return render(request, 'task_create.html')
+        return render(request, 'task_create.html', {'status': status_choices})
     elif request.method == "POST":
+        date_done = request.POST.get('date_done')
+        if not date_done:
+            date_done = None
         Tasks.objects.create(
-            description=request.POST['description'],
-            status=request.POST['status'],
-            date_done=request.POST['date_done']
+            description=request.POST.get('description'),
+            status=request.POST.get('status'),
+            date_done=date_done
         )
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect('/task_page')
